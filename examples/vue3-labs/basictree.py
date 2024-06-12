67381d04d6242602ff482a5c14ecc3a711516443
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2024-06-12 09:18:30 -0600

    docs(lab): add another tree example

---
+++ examples/vue3-labs/basictree.py
@@ -0,0 +1,55 @@
+from trame.app import get_server
+from trame.widgets import vuetify3
+from trame.ui.vuetify3 import SinglePageLayout
+import itertools
+import random
+
+
+# -----------------------------------------------------------------------------
+# Trame setup
+# -----------------------------------------------------------------------------
+
+server = get_server(client_type="vue3")
+state, ctrl = server.state, server.controller
+
+vuetify3.enable_lab()
+
+newid = itertools.count().__next__
+
+MAX_DEPTH = 5
+
+
+def generate_node(n_children, depth=0):
+    key = newid()
+    children = []
+    if depth < MAX_DEPTH:
+        children = [
+            generate_node(random.randint(0, 5), depth + 1) for i in range(n_children)
+        ]
+
+    if len(children):
+        return dict(id=key, title=f"Node {key}", children=children)
+
+    return dict(id=key, title=f"Node {key}")
+
+
+TREE = [generate_node(random.randint(0, 5), 0) for i in range(5)]
+
+
+def new_tree_select(event):
+    print(event)
+
+
+with SinglePageLayout(server) as layout:
+    with layout.content:
+        vuetify3.VTreeview(
+            item_value="id",
+            items=("tree", TREE),
+            click_select=(
+                new_tree_select,
+                "[$event]",
+            ),  # Emits the item when it is clicked to select.)
+        )
+
+if __name__ == "__main__":
+    server.start()
