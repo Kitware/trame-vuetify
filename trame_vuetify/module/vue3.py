5ea6a5387d02a500876957ed519b163f11e62402
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2025-08-04 12:59:16 -0600

    fix(roboto): use self served roboto fonts

--- trame_vuetify/module/vue3.py
+++ trame_vuetify/module/vue3.py
@@ -2,12 +2,17 @@ from pathlib import Path
 from trame_vuetify import __version__
 
 serve_path = str(Path(__file__).with_name("vue3-serve").resolve())
-serve = {f"__trame_vuetify3_{__version__}": serve_path}
+roboto_path = str(Path(__file__).with_name("roboto").resolve())
+
+serve = {
+    f"__trame_vuetify3_{__version__}": serve_path,
+    f"__trame_roboto_{__version__}": roboto_path,
+}
 scripts = [f"__trame_vuetify3_{__version__}/vuetify3.js"]
 styles = [
     f"__trame_vuetify3_{__version__}/vuetify3.css",
     f"__trame_vuetify3_{__version__}/css/mdi.css",
-    "https://fonts.googleapis.com/css?family=Roboto:300,400,500",
+    f"__trame_roboto_{__version__}/roboto.css",
 ]
 vue_use = ["Vuetify.createVuetify(trame.state.get('trame__vuetify3_config')||{})"]
 
--- trame_vuetify/module/vue3_lab.py
+++ trame_vuetify/module/vue3_lab.py
@@ -2,12 +2,18 @@ from pathlib import Path
 from trame_vuetify import __version__
 
 serve_path = str(Path(__file__).with_name("vue3-lab-serve").resolve())
-serve = {f"__trame_vuetify_lab_{__version__}": serve_path}
+roboto_path = str(Path(__file__).with_name("roboto").resolve())
+
+serve = {
+    f"__trame_vuetify_lab_{__version__}": serve_path,
+    f"__trame_roboto_{__version__}": roboto_path,
+}
+
 scripts = [f"__trame_vuetify_lab_{__version__}/trame-vuetify-lab.umd.js"]
 styles = [
     f"__trame_vuetify_lab_{__version__}/style.css",
     f"__trame_vuetify_lab_{__version__}/css/mdi.css",
-    "https://fonts.googleapis.com/css?family=Roboto:300,400,500",
+    f"__trame_roboto_{__version__}/roboto.css",
 ]
 vue_use = ["vuetifylab.createVuetify(trame.state.get('trame__vuetify3_config')||{})"]
 
