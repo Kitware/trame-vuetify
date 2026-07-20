1db751522e2bb57c884d63b592234f82379c034e
Tyler Walker <tyler.walker7@protonmail.com>
2026-07-20 08:02:22 -0400

    fix(generate-python): Be more specific on url portion to replace

--- js-libs/v3/generate_python.py
+++ js-libs/v3/generate_python.py
@@ -136,7 +136,9 @@ def split_description(description):
 def get_docs(tag):
     url = tag.get("doc-url", "https://vuetifyjs.com/en/introduction/why-vuetify/")
     url = url.replace("www.", "")  # www redirects to start page
-    url = url.replace("vuetifyjs", "v3.vuetifyjs")  # the hosted file points to v4 docs
+    url = url.replace(
+        "/vuetifyjs", "/v3.vuetifyjs"
+    )  # the hosted file points to v4 docs
 
     name = tag.get("name")
     attributes = tag.get("attributes", [])
