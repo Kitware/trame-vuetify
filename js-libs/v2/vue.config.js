c74fcc6091682a3a272ded92a08f06661715338d
Tyler Walker <tyler.walker7@protonmail.com>
2026-07-20 08:00:23 -0400

    ci: Fix paths to navigate during build

--- js-libs/v2/vue.config.js
+++ js-libs/v2/vue.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const DST_PATH = '../trame_vuetify/module/v2-serve';
+const DST_PATH = '../../trame_vuetify/module/v2-serve';
 
 module.exports = {
   outputDir: path.resolve(__dirname, DST_PATH),
